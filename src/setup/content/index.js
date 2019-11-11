71b23620ac5e73a27ed6b6c5eb6a7390f38a18c3
aleksey.lipkin <aleksey1lipkin@gmail.com>
2019-11-11 22:57:29 +0400

    add replace location

--- src/setup/content/index.js
+++ src/setup/content/index.js
@@ -1,1 +1,19 @@
-console.log('hello');
+import { getFromChromeStore } from 'src/utils/chrome';
+import { CHROME_STORE_KEYS } from 'src/utils/constants';
+
+async function compareHrefWithConfig() {
+  const { href } = window.location;
+  const redirectConfig = await getFromChromeStore(
+    CHROME_STORE_KEYS.redirectSettings,
+  );
+  const sourceMatchingConfig = Object.values(redirectConfig).find(({ from }) =>
+    href.includes(from),
+  );
+  if (sourceMatchingConfig) {
+    const { from, to } = sourceMatchingConfig;
+    const newHref = href.replace(from, to);
+    window.location.replace(newHref);
+  }
+}
+
+compareHrefWithConfig();
